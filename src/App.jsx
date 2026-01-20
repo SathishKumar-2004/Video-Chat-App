c999accd16d06169ed28926730c67971381006df
Sathish Kumar S <sathishkumarvk227@gmail.com>
2026-01-20 11:53:34 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { io } from 'socket.io-client'
+import './App.css'
+import VideoChat from './VideoChat'
+const App = () => {
+
+  return (
+  <VideoChat/>
+)
+  
+}
+
+export default App
